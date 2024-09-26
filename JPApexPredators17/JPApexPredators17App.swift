50ba26930a897209050dd266a3f28d70dcbcef7e
Mark Santoro <mark_santoro@comcast.net>
2024-09-25 18:53:19 -0700

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Mark Santoro on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
