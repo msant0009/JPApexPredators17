f9145c2b7f7b26e7e91aa61271fc711fa585aab5
Mark Santoro <mark_santoro@comcast.net>
2024-10-01 19:34:09 -0700

    Map styles, positioning, and satellite views

--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -48,9 +48,7 @@ struct PredatorDetail: View {
                     
                     //current location
                     NavigationLink {
-                        Image(predator.image)
-                            .resizable()
-                            .scaledToFit()
+                        PredatorMap(position:.camera(MapCamera(centerCoordinate: predator.location, distance: 1000, heading: 250, pitch: 80)))
                         
                     }label: {
                         Map(position: $position){
